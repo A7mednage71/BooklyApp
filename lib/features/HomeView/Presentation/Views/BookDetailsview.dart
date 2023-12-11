d4b129b34403b3b1ada1d802d92dba4a06bcadbf
A7med_Nage7 <ahmdnajkh71@gmail.com>
2023-12-12 00:54:06 +0200

    create Book Details

---
+++ lib/features/HomeView/Presentation/Views/BookDetailsview.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/features/HomeView/Presentation/Views/wedgets/BookDetailsBody.dart';
+import 'package:flutter/material.dart';
+
+class BookDatailsView extends StatelessWidget {
+  const BookDatailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BookDetailsBody();
+  }
+}
