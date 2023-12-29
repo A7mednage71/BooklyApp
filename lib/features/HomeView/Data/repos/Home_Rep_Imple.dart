cad72a70be642b8c00c9700552653560d35b6655
A7med_Nage7 <ahmdnajkh71@gmail.com>
2023-12-30 00:26:29 +0200

    create server failure from DioException

--- lib/features/HomeView/Data/repos/Home_Rep_Imple.dart
+++ lib/features/HomeView/Data/repos/Home_Rep_Imple.dart
@@ -1,15 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
 
-
+import 'package:bookly/core/Utils/Api_Service.dart';
 import 'package:bookly/core/errors/Failurs.dart';
 import 'package:bookly/features/HomeView/Data/Models/book_model.dart';
 import 'package:bookly/features/HomeView/Data/repos/Home_repo.dart';
-import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoImpl implements HomeRepository {
+class HomeRepoImpl implements HomeRepo {
+  ApiService apiService;
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchnewestBooks() async {
+    try {
+      var data = await apiService.get(
+          EndPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
+      return right(data as List<BookModel>);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
@@ -17,5 +30,4 @@ class HomeRepoImpl implements HomeRepository {
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
-
 }
--- lib/features/HomeView/Data/repos/Home_repo.dart
+++ lib/features/HomeView/Data/repos/Home_repo.dart
@@ -2,7 +2,7 @@ import 'package:bookly/core/errors/Failurs.dart';
 import 'package:bookly/features/HomeView/Data/Models/book_model.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class HomeRepository {
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchnewestBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
