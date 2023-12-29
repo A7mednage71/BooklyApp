cdb0b088e9be7b913ca3e40f852b5f2ca7b9ab03
A7med_Nage7 <ahmdnajkh71@gmail.com>
2023-12-29 16:08:22 +0200

    private api Service

--- lib/core/Utils/Api_Service.dart
+++ lib/core/Utils/Api_Service.dart
@@ -1,14 +1,13 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-  final baseUrl = 'https://www.googleapis.com/books/v1/';
-  final Dio dio;
-  ApiService(
-    this.dio,
-  );
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final Dio _dio;
+  ApiService(this._dio);
+
   // Endpoints is the end of Url
   Future<Map<String, dynamic>> get({required String EndPoint}) async {
-    var response = await dio.get('$baseUrl$EndPoint');
+    var response = await _dio.get('$_baseUrl$EndPoint');
     return response.data;
   }
 }
